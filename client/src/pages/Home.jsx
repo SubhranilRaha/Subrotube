e690f4193cb67405a01c69371ff38d0e3d25506f
SubhranilRaha <82709110+SubhranilRaha@users.noreply.github.com>
2023-03-25 00:44:46 +0530

    cors and api

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = ({type}) => {
 
   useEffect(() => {
     const fetchVideos = async () => {
-      const res = await axios.get(`/videos/${type}`);
+      const res = await axios.get(`https://subrotubeapi.onrender.com/api/videos/${type}`);
       setVideos(res.data);
       console.log(res.data)
     };
