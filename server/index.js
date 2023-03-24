d631e73029637ade76d60271d8ebd64fc72cdc18
SubhranilRaha <82709110+SubhranilRaha@users.noreply.github.com>
2023-03-25 00:42:11 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -6,9 +6,14 @@ import videoRoutes from "./routes/videos.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
-
+import cors from 'cors'
 
 const app = express();
+
+app.use(cors({
+  origin: "*",
+}))
+
 dotenv.config();
 const PORT=process.env.PORT||8800
 
